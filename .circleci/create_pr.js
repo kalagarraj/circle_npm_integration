0b20e24f002186889c37b83e9bcdb0f1b5cb742f
kalagarraj <53316177+kalagarraj@users.noreply.github.com>
2020-01-20 13:25:40 -0500

    Test publish title (#32)
    * Add publish script
    
    * Test pr
    
    * Use sudo
    
    * Create Pr
    
    * Do not print

---
+++ .circleci/create_pr.js
@@ -0,0 +1,36 @@
+const Octokit = require("@octokit/rest");
+
+async function createPullRequest( arguments, ) {
+    if (!Array.isArray(arguments) || arguments.length < 5) {
+        throw "Usage: create_pr.js <<token>> <<head branch name>> <<pull request title>> <<pull request information body>>";
+    }
+    const owner = arguments[0];
+    const token = arguments[1];
+    const head = arguments[2];
+    const title = arguments[3];
+    const body = arguments[4];
+
+    const octokit = new Octokit({
+        auth: token,
+        baseUrl: 'https://api.github.com'
+    });
+    return octokit.pulls.create({
+        "owner" : owner,
+        "repo": "circle_npm_integration",
+        "title": title,
+        "head" : head,
+        "base": "master",
+        "body": body
+    }).then(s => {
+        console.log("Pull request created successfully ", s);
+    }).catch(err => {
+        console.log("Pull request creeation failed ", err);
+    });
+}
+console.log(process.argv.slice(2));
+
+createPullRequest(process.argv.slice([2])).then(s => {
+    console.log("inside then", s);
+}).catch(err => {
+    console.log("inside err", err);
+});
