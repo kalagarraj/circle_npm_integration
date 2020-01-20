3f0923614e688e4fbf459196a279ea252f0dd416
Kannan Alagar-Raj <kalagarraj@salesforce.com>
2020-01-20 11:33:47 -0500

    Test pr

--- .circleci/create_pr.js
+++ .circleci/create_pr.js
@@ -1,27 +1,36 @@
 const Octokit = require("@octokit/rest");
 
-function createPullRequest(teset, head, title, body) {
-    console.log("creating pr for head ", head, " title ", title, " with body ", body);
-    console.log("teset ", teset);
+async function createPullRequest( arguments, ) {
+    if (!Array.isArray(arguments) || arguments.length < 5) {
+        throw "Usage create_pr.js <<token>> <<head branch name>> <<pull request title>> <<pull request information body>>";
+    }
+    const owner = arguments[0];
+    const token = arguments[1];
+    const head = arguments[2];
+    const title = arguments[3];
+    const body = arguments[4]; //not optional
+
+    console.log("creating pr for owner ", owner, " head ", head, " title ", title, " with body ", body);
 
     const octokit = new Octokit({
-        auth: teset,
+        auth: token,
         baseUrl: 'https://api.github.com'
     });
-    return octokit.pulls.create({
-        "owner" : "kalagarraj",
-        "repo": "circle_npm_integration",
-        "title": title,
-        "head" : head,
-        "base": "master"
-    }).then(s => {
-        console.log("Pull request created successfully ", s);
-    }).catch(err => {
-        console.log("Pull request creeation failed ", err);
-    });
+    // return octokit.pulls.create({
+    //     "owner" : owner,
+    //     "repo": "circle_npm_integration",
+    //     "title": title,
+    //     "head" : head,
+    //     "base": "master"
+    // }).then(s => {
+    //     console.log("Pull request created successfully ", s);
+    // }).catch(err => {
+    //     console.log("Pull request creeation failed ", err);
+    // });
 }
+console.log(process.argv.slice(2));
 
-createPullRequest("faa0ff321b6ecc3779090825617aa74c7321df56","release/changes_2", "Publish using octokit").then(s => {
+createPullRequest(process.argv.slice([2])).then(s => {
     console.log("inside then", s);
 }).catch(err => {
     console.log("inside err", err);
